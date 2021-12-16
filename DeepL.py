f10486be4a7a1d4baaa347609ccf83363e0645b3
Miguel <miguelmartinez_95@outlook.es>
2021-12-16 08:17:05 +0100

    modification for improving the cryto lstm (training)

--- DeepL.py
+++ DeepL.py
@@ -560,8 +560,8 @@ class LSTM_model(DL):
                 model.add(LSTM(neurons_lstm[k], batch_input_shape=(batch, n_timesteps, 1), stateful=True,
                          return_sequences=True))
             elif k==layers_lstm-1:
-                model.add(LSTM(neurons_lstm[k],  batch_input_shape=(batch, n_timesteps, 1), stateful=True))
-                model.add(TimeDistributed(Dense(1)))
+                #model.add(LSTM(neurons_lstm[k],  batch_input_shape=(batch, n_timesteps, 1), stateful=True))
+                model.add(LSTM(neurons_lstm[k],  batch_input_shape=(batch, n_timesteps, 1)
            # else:
            #     model.add(LSTM(neurons_lstm[k], activation='relu'))
       #  for z in range(layers_neurons):
