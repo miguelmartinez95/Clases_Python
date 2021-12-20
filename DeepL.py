f83c9bc7788c676fb98410cdf767142f751cfff6
Miguel <miguelmartinez_95@outlook.es>
2021-12-20 23:31:49 +0100

    elimination of -1 with range

--- DeepL.py
+++ DeepL.py
@@ -725,8 +725,8 @@ class LSTM_model(DL):
            for i in range(2):
                 train, test, index_test = LSTM_model.split_dataset(data, n_lags,w, w2)
 
-                index_val = index_test[range(len(index_test)-math.ceil(len(index_test)/2), len(index_test)-1),:]
-                val = test[range(test.shape[0]-math.ceil(test.shape[0]/2), test.shape[0]-1),:,:]
+                index_val = index_test[range(len(index_test)-math.ceil(len(index_test)/2), len(index_test)),:]
+                val = test[range(test.shape[0]-math.ceil(test.shape[0]/2), test.shape[0]),:,:]
                 #test = test[range(test.shape[0]-math.floor(test.shape[0]/2), test.shape[0]),:,:]
                 test = test[range(0, math.ceil(test.shape[0] / 2)), :, :]
                 #index_test = index_test[range(test.shape[0]-math.ceil(test.shape[0]/2), test.shape[0]-1),:]
