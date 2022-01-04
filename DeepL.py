f207796dfcfc3a166766fc2252f3ebe87f30f27b
Miguel <miguelmartinez_95@outlook.es>
2022-01-04 21:41:35 +0100

    Improves in search_nsga2

--- DeepL.py
+++ DeepL.py
@@ -1533,7 +1533,7 @@ class MyProblem(LSTM_model, Problem):
 
         #igual tengo que meter todos los argumentos de LSTM_model
         super().__init__(data,horizont,scalar_y,scalar_x,zero_problem, limits,times, pos_y, mask,mask_value,n_lags,  inf_limit,sup_limit, repeat_vector,dropout, type)
-        Problem.__init__(n_var=self.n_var,
+        super().__init__(n_var=self.n_var,
                          n_obj=2,
                          n_constr=1,
                          xl=self.xlimit_inf,
