c9fc0c36d74188169b4399392872f58ca7f3d674
Miguel <miguelmartinez_95@outlook.es>
2022-01-21 17:21:05 +0100

    change in MLP series

--- ML.py
+++ ML.py
@@ -233,14 +233,12 @@ class ML:
                 X = X.drop(X.index[X.shape[0] - 1], axis=0)
 
                 index1 =X.index
-
                 y,gap = self.cortes_onebyone(y, len(y), self.n_steps)
                 y=pd.DataFrame(y.transpose())
 
-
                 if gap>0:
                     X=X.drop(X.index[range(X.shape[0] - 1, X.shape[0])], axis=0)
-                    index1 = np.delete(index1, range(X.shape[0] - 1, X.shape[0]))
+                    index1 = np.delete(index1, range(X.shape[0] -1, X.shape[0]))
 
                 X = X.drop(X.index[range(X.shape[0] - self.n_steps+1, X.shape[0])], axis=0)
                 index1 = np.delete(index1, range(len(index1)-self.n_steps+1, len(index1)))
