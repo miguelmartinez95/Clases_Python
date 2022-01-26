e039d3abb1df68d765f7c12235fdbf355c858157
Miguel <miguelmartinez_95@outlook.es>
2022-01-26 12:28:10 +0100

    modifying train and predict DL outside

--- DeepL.py
+++ DeepL.py
@@ -734,11 +734,15 @@ class LSTM_model(DL):
            for i in range(2):
                 train, test, index_test = LSTM_model.split_dataset(data, n_lags,w, w2)
 
-                index_val = index_test[range(len(index_test)-math.ceil(len(index_test)/2), len(index_test))]
+                index_val = index_test[range(index_test.shape[0]-math.ceil(index_test.shape[0]/2), index_test.shape[0])]
                 #index_val = index_test[range(index_test.shape[0]-math.ceil(index_test.shape[0]/2)),: ,1]
                 val = test[range(test.shape[0]-math.ceil(test.shape[0]/2), test.shape[0]),:,:]
                 test = test[range(0, math.ceil(test.shape[0] / 2)), :, :]
                 x_train, y_train = LSTM_model.to_supervised(train, pos_y, n_lags,horizont)
+
+                print(index_val.shape)
+                print(val.shape)
+
 
                 x_test, y_test = LSTM_model.to_supervised(test, pos_y, n_lags,horizont)
                 x_val, y_val = LSTM_model.to_supervised(val, pos_y, n_lags,horizont)
