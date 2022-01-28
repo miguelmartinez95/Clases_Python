39410ca6c9059d830b6bb2541430b5fe16283315
Miguel <miguelmartinez_95@outlook.es>
2022-01-28 14:51:17 +0100

    improving DL

--- ML.py
+++ ML.py
@@ -227,7 +227,7 @@ class ML:
         '''
         Move the data sample to connected the y with the x based on the future selected
         '''
-        if self.steps==0:
+        if self.n_steps==0:
             self.data = self.data
         else:
             if self.type=='series':
