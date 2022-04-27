a9ad9214d23c8af0cadc85fe04bf75dca32309cb
Miguel <miguelmartinez_95@outlook.es>
2022-04-27 11:09:54 +0200

    errors modification in how input predict and real

--- errors.py
+++ errors.py
@@ -30,6 +30,7 @@ class Eval_metrics:
             for i in range(self.real.shape[1]):
                 print(self.real.shape)
                 print(self.predict.shape)
+                print(self.real)
                 cv[i] = 100 * (np.sqrt(metrics.mean_squared_error(self.real[:, i], self.predict[:, i])) / mean[i])
 
             cv= np.mean(cv)
