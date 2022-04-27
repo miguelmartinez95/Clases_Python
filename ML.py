0a53fba16121dab4366332f03a9cec024f2f5918
Miguel <miguelmartinez_95@outlook.es>
2022-04-27 09:58:39 +0200

    improve mlp

--- ML.py
+++ ML.py
@@ -1580,7 +1580,7 @@ class MyProblem_mlp(ElementwiseProblem):
         out["G"] = g1
         print(x)
         n_dense = x[range(self.l_dense)]*10
-        n_pacience = x[len(x) - 1]
+        n_pacience = x[len(x) - 1]*10
         f1, f2 = self.cv_nsga(self.data, 10, n_dense, n_pacience, self.batch, self.med, self.dictionary)
         print(
             '\n ############################################## \n ############################# \n ########################## EvaluaciÃ³n ',
