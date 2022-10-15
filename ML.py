3ef171101c31ee7265c4efe68ff622428d762797
Miguel <miguelmartinez_95@outlook.es>
2022-10-15 12:59:22 +0200

    introducing svm

--- ML.py
+++ ML.py
@@ -2092,12 +2092,50 @@ class SVM(ML):
         I want to predict the moment four steps in future
         '''
 
+    @staticmethod
+    def svm_cv_division(x,y, fold):
+        '''
+        Division de la muestra en trozos según fold para datos normales y no recurrentes
+
+        :param fold: division for cv_analysis
+        :return: data divided into train, test and validations in addition to the indexes division for a CV analysis
+        '''
+        x = x.reset_index(drop=True)
+        y = y.reset_index(drop=True)
+        X_test = []
+        X_train = []
+        Y_test = []
+        Y_train = []
+
+        step = int(x.shape[0]/fold)
+        w = 0
+        w2 = step
+        indexes = []
+        try:
+            while w2 < x.shape[0]:
+                X_test = x.iloc[range(w,w2)]
+                X_train.append(x.drop(range(w,w2)))
+
+                Y_test = y.iloc[range(w, w2)]
+                Y_train.append(y.drop(range(w, w2)))
+                indexes.append(np.array([w, w2]))
+                w = w2
+                w2 += step
+                if w2 > x.shape[0] and w < x.shape[0]:
+                    w2 = x.shape[0]
+        except:
+            raise NameError('Problems with the sample division in the cv classic')
+
+        res = {'x_test': X_test, 'x_train':X_train, 'y_test':Y_test, 'y_train':Y_train,
+            'indexes':indexes}
+
+        return res
 
     def SVM_training(self,layers, neurons, inputs, outputs, mask, mask__value):
         'WORK IN PROGRESS'
 
 
-    def SVM_training(self,data_train,C, epsilon, tol, save_model, model=[]):
+    def SVR_training(self,data_train,C, epsilon, tol, save_model, model=[]):
         now = str(datetime.now().microsecond)
         data_train = pd.DataFrame(data_train)
         x_train = data_train.drop(data_train.columns[self.pos_y], axis=1)
@@ -2597,9 +2635,274 @@ class SVM(ML):
         return res
 
 
+from pymoo.core.problem import ElementwiseProblem
+class MyProblem_svm(ElementwiseProblem):
+    def info(self):
+        print('Class to create a specific problem to use NSGA2 in architectures search.')
+    def __init__(self,data,horizont, scalar_y,scalar_x, zero_problem,limits, times, pos_y, n_lags,n_steps,
+                 mask, mask_value, inf_limit,sup_limit, type,med, contador,
+        n_var, C_max, epsilon_max, xlimit_inf, xlimit_sup, dictionary, weights, **kwargs):
+        super().__init__(n_var=n_var,
+                         n_obj=2,
+                         n_constr=0,
+                         xl=xlimit_inf,
+                         xu=xlimit_sup,
+                         type_var=np.int,
+                         # elementwise_evaluation=True,
+                         **kwargs)
+
+        self.data = data
+        self.horizont = horizont
+        self.scalar_y = scalar_y
+        self.scalar_x = scalar_x
+        self.zero_problem = zero_problem
+        self.limits = limits
+        self.times = times
+        self.pos_y = pos_y
+        self.mask = mask
+        self.mask_value = mask_value
+        self.n_lags = n_lags
+        self.inf_limit = inf_limit
+        self.sup_limit = sup_limit
+        self.type = type
+        self.med = med
+        self.contador = contador
+        self.C_max = C_max
+        self.epsilon_max = epsilon_max
+        self.xlimit_inf = xlimit_inf
+        self.xlimit_sup = xlimit_sup
+        self.n_var = n_var
+        self.dictionary = dictionary
+        self.weights = weights
+    @staticmethod
+    def complex_svm(C_svm,epsilon_svm, max_C, max_epsilon):
+        '''
+        :param max_N: maximun neurons in the network
+        :param max_H: maximum hidden layers in the network
+        :return: complexity of the model
+        '''
+
+        F = 0.75 * (C_svm / max_C) + 0.25 * (epsilon_svm/max_epsilon)
+        return F
+
+    def cv_opt(self,data, fold, C_svm,epsilon_svm, mean_y, dictionary):
+        '''
+        :param fold:assumed division of the sample for cv
+        :param dictionary: dictionary to fill with the options tested
+        :param q:operator to differentiate when there is parallelisation and the results must be a queue
+        :return: cv(rmse) and complexity of the model tested
+        if mean_y is empty a variation rate will be applied
+        '''
+
+        from pathlib import Path
+        import random
+
+        h_path = Path('./best_models')
+        h_path.mkdir(exist_ok=True)
+        h = h_path / f'best_{random.randint(0, 1000000)}_model.h5'
+
+        name1 = tuple(np.concatenate((np.aarray([C_svm]), np.array([epsilon_svm]))))
+        try:
+            a0, a1 = dictionary[name1]
+            return a0, a1
+        except KeyError:
+            pass
+        cvs = [0 for x in range(fold)]
+        print(type(data))
+        names = self.data.columns
+        names = np.delete(names, self.pos_y)
+
+        y = self.data.iloc[:,self.pos_y]
+        x =self.data.drop(self.data.columns[self.pos_y],axis=1)
+
+        res = SVM.svm_cv_division(x, y, fold)
+        x_test = res['x_test']
+        x_train = res['x_train']
+        y_test = res['y_test']
+        y_train = res['y_train']
+        indexes = res['indexes']
+        times_test = []
+        tt = self.times
+        for t in range(len(indexes)):
+            times_test.append(tt[indexes[t][0]:indexes[t][1]])
+        if self.type=='classification':
+            data2 = self.data
+            #yy = data2.iloc[:, self.pos_y]
+            #yy = pd.Series(yy, dtype='category')
+            #n_classes = len(yy.cat.categories.to_list())
+            #model = SVM.SVM_training(layers, neurons, x_train[0].shape[1], n_classes, self.mask, self.mask_value)
+            ####################################################################
+            # EN PROCESOO ALGÚN DíA !!!!!!!
+            ##########################################################################
+        else:
+
+                       # Checkpoitn callback
+            es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=pacience)
+            mc = ModelCheckpoint(str(h), monitor='val_loss', mode='min', verbose=1, save_best_only=True)
+            # Train the model
+            for z in range(fold):
+                print('Fold number', z)
+                x_t = pd.DataFrame(x_train[z]).reset_index(drop=True)
+                y_t = pd.DataFrame(y_train[z]).reset_index(drop=True)
+                test_x = pd.DataFrame(x_test[z]).reset_index(drop=True)
+                test_y = pd.DataFrame(y_test[z]).reset_index(drop=True)
+                model = SVM.SVR_training(pd.concat([y_train[z], x_train[z]], axis=1), C_svm, epsilon_svm,tol,False)
+
+                model.fit(x_t, y_t, epochs=2000, validation_data=(test_x, test_y), callbacks=[es, mc], batch_size=batch)
+                y_pred = model.predict(test_x)
+                y_pred = np.array(self.scalar_y.inverse_transform(pd.DataFrame(y_pred)))
+                y_real = test_y
+                y_real2 = np.array(y_real.copy())
+                y_real = np.array(self.scalar_y.inverse_transform(y_real))
+                y_pred[np.where(y_pred < self.inf_limit)[0]] = self.inf_limit
+                y_pred[np.where(y_pred > self.sup_limit)[0]] = self.sup_limit
+                y_predF = y_pred.copy()
+                y_predF = pd.DataFrame(y_predF)
+                y_predF.index = times_test[z]
+                y_realF = y_real.copy()
+                y_realF = pd.DataFrame(y_realF)
+                y_realF.index = times_test[z]
+                if self.zero_problem == 'schedule':
+                    print('*****Night-schedule fixed******')
+                    res = ML.fix_values_0(times_test[z],
+                                               self.zero_problem, self.limits)
+                    index_hour = res['indexes_out']
+                    if len(index_hour) > 0 and self.horizont == 0:
+                        y_pred1 = np.delete(y_pred, index_hour, 0)
+                        y_real1 = np.delete(y_real, index_hour, 0)
+                        y_real2 = np.delete(y_real2, index_hour, 0)
+                    elif len(index_hour) > 0 and self.horizont > 0:
+                        y_pred1 = np.delete(y_pred, index_hour - self.horizont, 0)
+                        y_real1 = np.delete(y_real, index_hour - self.horizont, 0)
+                        y_real2 = np.delete(y_real2, index_hour - self.horizont, 0)
+                    else:
+                        y_pred1 = y_pred
+                        y_real1 = y_real
+                    if self.mask == True:
+                        # Outliers and missing values
+                        o = np.where(y_real2 < self.inf_limit)[0]
+                        if len(o) > 0:
+                            y_pred1 = np.delete(y_pred1, o, 0)
+                            y_real1 = np.delete(y_real1, o, 0)
+                    if np.sum(np.isnan(y_pred1)) == 0 and np.sum(np.isnan(y_real1)) == 0:
+                        if mean_y.size == 0:
+                            e=evals(y_pred1, y_real1).variation_rate()
+                            if isinstance(self.weights, list):
+                                cvs[z]=np.sum(e)
+                            else:
+                                cvs[z] = np.sum(e * self.weights)
+
+                        else:
+                            e = evals(y_pred1, y_real1).cv_rmse(mean_y)
+                            if isinstance(self.weights, list):
+                                cvs[z] = np.sum(e)
+                            else:
+                                cvs[z] = np.sum(e * self.weights)
+
+                elif self.zero_problem == 'radiation':
+
+                    print('*****Night-radiation fixed******')
+                    place = np.where(names == 'radiation')[0]
+                    scalar_rad = self.scalar_x['radiation']
+                    res = ML.fix_values_0(scalar_rad.inverse_transform(x_test[z].iloc[:, place]),
+                                               self.zero_problem, self.limits)
+                    index_rad = res['indexes_out']
+                    if len(index_rad) > 0 and self.horizont == 0:
+                        y_pred1 = np.delete(y_pred, index_rad, 0)
+                        y_real1 = np.delete(y_real, index_rad, 0)
+                        y_real2 = np.delete(y_real2, index_rad, 0)
+                    elif len(index_rad) > 0 and self.horizont > 0:
+                        y_pred1 = np.delete(y_pred, index_rad - self.horizont, 0)
+                        y_real1 = np.delete(y_real, index_rad - self.horizont, 0)
+                        y_real2 = np.delete(y_real2, index_rad - self.horizont, 0)
+                    else:
+                        y_pred1 = y_pred
+                        y_real1 = y_real
+                    if self.mask == True:
+                        # Outliers and missing values
+                        o = np.where(y_real2 < self.inf_limit)[0]
+                        if len(o) > 0:
+                            y_pred1 = np.delete(y_pred1, o, 0)
+                            y_real1 = np.delete(y_real1, o, 0)
+                        else:
+                            y_pred1 = y_pred
+                            y_real1 = y_real
+                    if np.sum(np.isnan(y_pred1)) == 0 and np.sum(np.isnan(y_real1)) == 0:
+                        if mean_y.size == 0:
+                            e=evals(y_pred1, y_real1).variation_rate()
+                            if isinstance(self.weights, list):
+                                cvs[z] = np.sum(e)
+                            else:
+                                cvs[z] = np.sum(e * self.weights)
+
+                        else:
+                            e = evals(y_pred1, y_real1).cv_rmse(mean_y)
+                            if isinstance(self.weights, list):
+                                cvs[z] = np.sum(e)
+                            else:
+                                cvs[z] = np.sum(e * self.weights)
+
+                    else:
+                        print('Missing values are detected when we are evaluating the predictions')
+                        cvs[z] = 9999
+                else:
+                    if self.mask == True:
+                        # Outliers and missing values
+                        o = np.where(y_real2 < self.inf_limit)[0]
+                        if len(o) > 0:
+                            y_pred2 = np.delete(y_pred, o, 0)
+                            y_real2 = np.delete(y_real, o, 0)
+                        else:
+                            y_pred2 = y_pred
+                            y_real2 = y_real
+                    else:
+                        y_pred2 = y_pred
+                        y_real2 = y_real
+                    if np.sum(np.isnan(y_pred2)) == 0 and np.sum(np.isnan(y_real2)) == 0:
+                        if mean_y.size == 0:
+                            e=evals(y_pred2, y_real2).variation_rate()
+                            if isinstance(self.weights, list):
+                                cvs[z] = np.sum(e)
+                            else:
+                                print(e)
+                                print(self.weights)
+                                cvs[z] = np.sum(e * self.weights)
+
+                        else:
+                            e = evals(y_pred2, y_real2).cv_rmse(mean_y)
+                            if isinstance(self.weights, list):
+                                cvs[z] = np.sum(e)
+                            else:
+                                cvs[z] = np.sum(e * self.weights)
+
+                    else:
+                        print('Missing values are detected when we are evaluating the predictions')
+                        cvs[z] = 9999
+
+            complexity = MyProblem_svm.complex_mlp(C_svm, epsilon_svm, 10000, 100)
+            dictionary[name1] = np.mean(cvs), complexity
+            print(dictionary[name1] )
+            res_final = {'cvs': np.mean(cvs), 'complexity': complexity}
+            return res_final['cvs'], res_final['complexity']
+
+     def _evaluate(self, x, out, *args, **kwargs):
+        g1 = MyProblem_mlp.bool4(np.delete(x, len(x) - 1),self.l_dense)
+        out["G"] = g1
+        print(x)
+        n_dense = x[range(self.l_dense)]*20
+        n_pacience = x[len(x) - 1]*20
+        f1, f2 = self.cv_opt(self.data, 5, n_dense, n_pacience, self.batch, self.med, self.dictionary)
+        print(
+            '\n ############################################## \n ############################# \n ########################## EvaluaciÃ³n ',
+            self.contador, '\n #########################')
+        self.contador[0] += 1
+        out["F"] = np.column_stack([f1, f2])
+
+
 #class RF(ML):
 #    def info(self):
 #        print(('Class to built RF models. \n'))
 #    def __init__(self,data,scalar_y,scalar_x,zero_problem,limit,schedule,times):
 #        super().__init__(data,scalar_y, scalar_x,zero_problem,limit,schedule, times, pos_y)
 #
+
