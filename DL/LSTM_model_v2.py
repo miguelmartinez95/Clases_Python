46da1f999f751eb7de98908fd618f4d87727cb80
Miguel <miguelmartinez_95@outlook.es>
2022-12-11 17:39:00 +0100

    testing supervised-train

--- DL/LSTM_model_v2.py
+++ DL/LSTM_model_v2.py
@@ -1050,7 +1050,7 @@ class LSTM_model(DL):
             name='mlp'+now+'.h5'
             model_trained.save(name, save_format='h5')
         if testing==True:
-            res={'X_test':x_test, 'X_train':x_train, 'Y_test':y_test, 'Y_train':y_train, 'ind_test':ind_test, 'ind_train':ind_train}
+            res={'train':train, 'test':test, 'X_test':x_test, 'X_train':x_train, 'Y_test':y_test, 'Y_train':y_train, 'ind_test':ind_test, 'ind_train':ind_train}
         else:
             res = {'model': model_trained, 'times': times, 'history':history}
         return res
