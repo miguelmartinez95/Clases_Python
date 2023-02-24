5332cc416a95cbe3925eb92d89d6ba721bba1053
Miguel <miguelmartinez_95@outlook.es>
2023-02-24 07:22:04 +0100

    hypervolume

--- DL/LSTM_model_v2.py
+++ DL/LSTM_model_v2.py
@@ -1729,6 +1729,50 @@ class LSTM_model(DL):
         plt.show()
         plt.savefig("convergence1.png")
 
+        ###############################################################################
+        X, F = res.opt.get("X", "F")
+        n_evals = []  # corresponding number of function evaluations\
+        hist_F = []  # the objective space values in each generation
+        hist_cv = []  # constraint violation in each generation
+        hist_cv_avg = []  # average constraint violation in the whole population
+        hist = res.history
+        for algo in hist:
+            # store the number of function evaluations
+            n_evals.append(algo.evaluator.n_eval)
+
+            # retrieve the optimum from the algorithm
+            opt = algo.opt
+
+            # store the least contraint violation and the average in each population
+            hist_cv.append(opt.get("CV").min())
+            hist_cv_avg.append(algo.pop.get("CV").mean())
+
+            # filter out only the feasible and append and objective space values
+            feas = np.where(opt.get("feasible"))[0]
+            hist_F.append(opt.get("F")[feas])
+
+        approx_ideal = F.min(axis=0)
+        approx_nadir = F.max(axis=0)
+        from pymoo.indicators.hv import Hypervolume
+
+        metric = Hypervolume(ref_point=np.array([1.1, 1.1]),
+                             norm_ref_point=False,
+                             zero_to_one=True,
+                             ideal=approx_ideal,
+                             nadir=approx_nadir)
+
+        hv = [metric.do(_F) for _F in hist_F]
+
+        plt.figure(figsize=(10, 7))
+        plt.plot(n_evals, hv, color='black', lw=0.7, label="Avg. CV of Pop")
+        plt.scatter(n_evals, hv, facecolor="none", edgecolor='black', marker="p")
+        plt.title("Convergence")
+        plt.xlabel("Function Evaluations")
+        plt.ylabel("Hypervolume")
+        plt.show()
+        plt.savefig("convergence_HV.png")
+        ###############################################################################
+
         print('The number of evaluations were:', contador)
         if n_processes>1:
             pool.close()
