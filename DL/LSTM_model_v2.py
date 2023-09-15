7e3a11c84ebb34e41a40b68fb475e924abeb0821
Miguel <miguelmartinez_95@outlook.es>
2023-09-15 11:05:04 +0200

    documentation

--- DL/LSTM_model_v2.py
+++ DL/LSTM_model_v2.py
@@ -67,14 +67,13 @@ class LSTM_model(DL):
         Subclass of DL 
         '''
 
-    def __init__(self, data, horizont,scalar_y, scalar_x,zero_problem, limits,times, pos_y, mask,mask_value,n_lags,n_steps,  inf_limit,sup_limit,names,extract_cero, repeat_vector,dropout,weights, type,
+    def __init__(self, data, horizont,scalar_y, scalar_x,zero_problem, limits,times, pos_y, mask,mask_value,n_lags,n_steps, inf_limit,sup_limit,names,extract_cero, repeat_vector,dropout,weights, type,
                  optimizer='adam',learning_rate=0.001,activation='relu'):
-        super().__init__(data, horizont,scalar_y, scalar_x,zero_problem, limits,times, pos_y, mask,mask_value,n_lags,  inf_limit,sup_limit,names,extract_cero)
+        super().__init__(data, horizont,scalar_y, scalar_x,zero_problem, limits,times, pos_y, mask,mask_value,n_lags,n_steps, inf_limit,sup_limit,names,extract_cero)
         self.repeat_vector = repeat_vector
         self.dropout = dropout
         self.type = type
         self.weights=weights
-        self.n_steps=n_steps
         self.activation = activation
         self.learning_rate = learning_rate
         self.optimizer = optimizer
